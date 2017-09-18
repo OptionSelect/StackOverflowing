b92beb89171c36a200cd39e1c15a96836786bae7
Brandyn Sullins <woofenwauzer@gmail.com>
2017-09-18 17:02:53 -0400

    Questions on the Front Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,17 +4,31 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StackOverflowing.Data;
 using StackOverflowing.Models;
 
 namespace StackOverflowing.Controllers
 {
+        
+
+        
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> um)
+        {
+            _context = context;
+            _userManager = um;
+        }
+
         [AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Questions.ToList());
         }
         public IActionResult Error()
         {
